dc165cb26223f2eb5b957285c4afb91eecfaaf7c
kentos <kent@kentcederstrom.se>
2016-10-07 12:02:16 +0200

    Kill some stuff

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 module.exports = require('./lib');
-
-var MongoStream = require('mongo-trigger');
-
-var watcher = new MongoStream({format: 'pretty'});
-
-// watch the collection
-watcher.watch('test.test', function(event) {
-  // parse the results
-  console.log('something changed:', event);
-});
